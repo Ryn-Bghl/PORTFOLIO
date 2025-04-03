fbc044cbb82073dd1718a18b4fd6f29ad17bed79
_Duck-Noris_ <rayan.boughalia@gmail.com>
2025-04-03 19:25:04 +0200

    Implement email sending functionality and improve layout for contact section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 function sendMail() {
-  // const from = document.querySelector("")
-  // const to =
-  // const body =
-  // const subject =
+  const email = document.querySelector(
+    "div.form-example:nth-child(4) > input:nth-child(2)"
+  ).value;
+  const recipient = "rayan.boughalia@gmail.com";
+  const message = document.querySelector("#msg").value;
+
+  alert("email: " + email + " message: " + message+ " recipient: " + recipient);
 }
